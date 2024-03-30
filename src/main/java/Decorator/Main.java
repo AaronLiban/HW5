c76ade96d42a01637839b5db003ce5b17b5736d0
Aaron Liban <Mocho23@outlook.com>
2024-03-29 22:59:43 -0700

    Made main have clearer messages

--- src/main/java/Decorator/Main.java
+++ src/main/java/Decorator/Main.java
@@ -8,15 +8,18 @@ public class Main {
 		Food meal1 = new Burger();
 		meal1 = new Ketchup(meal1);
 		order.addOrder(meal1);
+		System.out.println("Added burger with ketchup");
 
 		Food meal2 = new Fries();
 		meal2 = new Drink(meal2);
 		order.addOrder(meal2);
+		System.out.println("Added fries with drink");
 
 		Food meal3 = new Hotdog();
 		order.addOrder(meal3);
+		System.out.println("Added hotdog");
 
-		System.out.println(order.getPrice());
+		System.out.println("Total price of all orders: " + order.getPrice());
 
 	}
 }
